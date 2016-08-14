bb000cd4307d81876463380b4ee9d917091b7389
leheli <iszlai.lehel@gmail.com>
2016-08-14 13:26:21 +0200

    Model update

--- core/src/com/hexgame/model/Model.scala
+++ core/src/com/hexgame/model/Model.scala
@@ -1,8 +1,8 @@
 package com.hexgame.model
 
 
-
-
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.{SpriteBatch, Sprite}
 
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
@@ -11,43 +11,57 @@ import scala.collection.mutable.ListBuffer
   * Created by leheli on 2016.07.31..
   */
 
-class World {
+class World(batch:SpriteBatch) {
   val entities=new ListBuffer[Entity]
   val hive:Hive=new Hive
     hive.map.values.foreach( entities.+=)
-  val nextItem=new NexItem(Position(30,30,0),Side.getRandomSide())
+  val nextItem=nextItemBuilder()
+
+  def draw()={
+    for(i <- entities){
+      i.sprite.draw(batch)
+    }
+  }
+
+  private def nextItemBuilder():NexItem={
+    val side=Side.getRandomSide();
+    new NexItem(Position(30,30,0),side,new Texture(side.getTexture()))
+  }
+
 }
 
-abstract class Entity(val position: Position){
+abstract class Entity(val position: Position,var text:Texture) {
+  val sprite:Sprite=new Sprite(text)
   val events=new mutable.Queue[ToEntityEvent]()
   def handleEvents
 }
 
-class HexEntitie(pos: Position) extends Entity(pos){
+class HexEntitie(pos: Position,text:Texture) extends Entity(pos,text){
   private var state=HexState.BLANK
+
   def handleEvents:Unit={
    events.foreach(_ =>println("handling Event"))
  }
 
   def getNeighbourPosition(side:Side):Position  ={
     side match {
-      case Side.SIDE_A => Position(pos.x,pos.y,pos.z)
-      case Side.SIDE_B => Position(pos.x,pos.y,pos.z)
-      case Side.SIDE_C => Position(pos.x,pos.y,pos.z)
-      case Side.SIDE_D => Position(pos.x,pos.y,pos.z)
-      case Side.SIDE_E => Position(pos.x,pos.y,pos.z)
-      case Side.SIDE_F => Position(pos.x,pos.y,pos.z)
+      case Side.SIDE_A =>Position (pos.x, pos.y + 1,pos.z - 1)
+      case Side.SIDE_B =>Position (pos.x + 1,pos. y, pos.z - 1)
+      case Side.SIDE_C =>Position (pos.x + 1, pos.y - 1, pos.z)
+      case Side.SIDE_D =>Position (pos.x, pos.y - 1, pos.z + 1)
+      case Side.SIDE_E =>Position (pos.x - 1, pos.y, pos.z + 1)
+      case Side.SIDE_F =>Position (pos.x - 1, pos.y + 1,pos. z)
     }
   }
 }
 
-class NexItem(pos: Position,var side:Side) extends Entity(pos){
+class NexItem(pos: Position,var side:Side,text: Texture) extends Entity(pos,text){
   def handleEvents={
 
   }
 }
 
-case class Position(x:Byte,y:Byte,z:Byte)
+case class Position(x:Int,y:Int,z:Int)
 
 class Hive {
   val map = mutable.Map[Position,Entity]()
@@ -99,7 +113,7 @@ class Hive {
   )
 
   positions.foreach{ pos =>
-    val entry=new HexEntitie(pos)
+    val entry=new HexEntitie(pos,new Texture("hex.png"))
     map += (pos-> entry)
   }
 
